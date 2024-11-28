ccc76fdee28e45192ca53b1f3c06956b75a3f45a
jurek <jerzy.kunicki@gmail.com>
2024-11-28 01:50:13 +0100

    w main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import App  from "./App"
 import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
+import {createRoot} from "react-dom/client";
 import './index.css'
 
 
 
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<App/>
 		
