e9840c2268cd54e8e10bd68419a510f7a82ca133
jurek <jerzy.kunicki@gmail.com>
2024-12-02 01:09:08 +0100

    constructed sticky nav

---
+++ src/components/MainMenu.jsx
@@ -0,0 +1,26 @@
+import {  NavLink } from "react-router-dom";
+import {Outlet} from 'react-router';
+import "../style/Layout.css";
+
+const Layout = () => {
+	const links = [
+		{ id: 1, path: "/", name: "Home" },
+		{ id: 2, path: "/blogs", name: "Blogs" },
+		{ id: 3, path: "/albums", name: "Albums" },
+		{ id: 4, path: "/list", name: "List" },
+		{ id: 18, path: "/admin", name: "Admin panel" },
+	];
+
+	const items = links.map(item => {
+		return (
+			<div className='nav-menu ' key={item.id}>
+				<NavLink to={item.path}>{item.name}</NavLink>
+				<Outlet />
+			</div>
+		);
+	});
+
+	return <>{items}</>;
+};
+
+export default Layout;
